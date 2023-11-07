de06647a20ce65091567962d95c90a99bc6b157e
red-karthik <red.karthik558@gmail.com>
2023-11-06 22:30:46 -0500

    binary search added

--- Basic DSA/Searching Algorithms/binary_search.py
+++ Basic DSA/Searching Algorithms/binary_search.py
@@ -1,7 +1,12 @@
-import numpy
-
-def binary_search():
-    print("this is an algo for binary search")
-    pass
-
-binary_search()
+def binary_search(arr, search_num):
+    first, last = 0, len(arr) - 1
+    while first <= last:
+        mid = (first + last)// 2
+        if arr[mid] == search_num:
+            return mid
+        elif arr[mid] < search_num:
+            first = mid + 1
+        elif arr[mid] > search_num:
+            last = mid - 1
+    return -1
+print(binary_search([0,1,2,3,4,5,6],5))
